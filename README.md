5aee8d47c9d780676170929a775d02c7582536d3
Emil Hemdal <emil.hemdal.em@gmail.com>
2015-08-06 09:08:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cross-domain-login
+
+This is a test project to see if I'll be able to make cross domain login work in node.
+
+It does not use any database at this point.
---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * cross-domain-login
+ * Licence: AGPLv3
+ */
+
+var http = require('http')
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World\n')
+}).listen(36545)
